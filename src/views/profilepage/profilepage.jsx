f977ef14baf0702681b0f1e93debb8c0f25fcb43
Sandesh157-Th <ssahi563@rku.ac.in>
2024-02-24 15:49:25 +0530

    made some updation

--- src/views/profilepage/profilepage.jsx
+++ src/views/profilepage/profilepage.jsx
@@ -47,7 +47,7 @@ const ProfilePage = () => {
                   href=""
                   className="text-gray-500 dark:text-gray-300 hover:text-gray-900 hover:bg-gray-100 rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3"
                 >
-                  Twitter
+                  Twitter user
                 </a>
                 <a
                   href=""
