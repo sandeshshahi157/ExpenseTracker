ae29addac5e907542009226c321b32dd486732dc
Sandesh157-Th <ssahi563@rku.ac.in>
2024-02-28 20:25:22 +0530

    make some  arrangement

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import Root from "./routes/root";
-// import LandingPage from "./views/landingpage/landingpage";
-// import LoginForm from "./views/authentication/login";
-// import SignUpForm from "./views/authentication/signup";
+
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/mainRoutes";
 
